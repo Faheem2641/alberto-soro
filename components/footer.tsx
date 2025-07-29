f5d7631bbed6666635c46bcb1cfa7212184bb236
Faheem2641 <fali.ug24smme@student.nust.edu.pk>
2025-07-29 22:30:13 +0500

    Made change in footer year

--- components/footer.tsx
+++ components/footer.tsx
@@ -100,7 +100,7 @@ export function Footer() {
         </div>
 
         <div className="border-t border-black mt-8 pt-8 text-center text-gray-600">
-          <p>&copy; 2024 Alberto Sora. All rights reserved.</p>
+          <p>&copy; 2025 Alberto Sora. All rights reserved.</p>
         </div>
       </div>
     </footer>
